6a5eb8919d4164c2eca0d920b2c88e1c28d93a51
sibbngheid <sibbngheid@gmail.com>
2020-07-10 02:25:30 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="push">push vue</button>
+    <transition-group>
+      <div class="items" v-for="item in items" :key="item">{{item}}</div>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+      push_count: 0
+    }
+  },
+  methods: {
+    push(){
+      this.items.push("vue " + ++this.push_count)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.items {
+  background: #888;
+}
+</style>
